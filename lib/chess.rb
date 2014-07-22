65adc25df906499870709fc9e94906a56ce130b8
Wai Lee Chin Feman <skatenerd@gmail.com>
2014-07-21 23:01:57 -0500

    TABS

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,19 +1,19 @@
 require 'set'
 
 class Piece
-	attr_reader :color
+  attr_reader :color
 
   def initialize(color)
-		@color = color
+    @color = color
   end
 
-	def opponent?(other)
-		return (other.color != color)
-	end
+  def opponent?(other)
+    return (other.color != color)
+  end
 
-	def castles_with_king(king)
-		false
-	end
+  def castles_with_king(king)
+    false
+  end
 end
 
 class Position
@@ -23,47 +23,47 @@ class Position
     @col = col
   end
 
-	def ==(other)
-		return (other.row == row) && (other.col == col)
-	end
+  def ==(other)
+    return (other.row == row) && (other.col == col)
+  end
 end
 
 class Board
-	class NullPiece < Piece
-		def color
-		end
+  class NullPiece < Piece
+    def color
+    end
 
-		def moves(board)
-			[]
-		end
-	end
+    def moves(board)
+      []
+    end
+  end
 
   BLACK = :black
   WHITE = :white
   SIZE = 8
-	EMPTY = NullPiece.new(nil)
-	
+  EMPTY = NullPiece.new(nil)
+  
   attr_accessor :state, :moved_pieces
   def initialize
-		@moved_pieces = []
+    @moved_pieces = []
     @state = (0...SIZE).map do
-			(0...SIZE).map do
-				EMPTY
-			end
-		end
+      (0...SIZE).map do
+        EMPTY
+      end
+    end
   end
 
-	def on_board(position)
-		row_ok = position.row >= 0 && position.row < SIZE
-	  col_ok = position.col >= 0 && position.col < SIZE
-		row_ok && col_ok
-	end
+  def on_board(position)
+    row_ok = position.row >= 0 && position.row < SIZE
+    col_ok = position.col >= 0 && position.col < SIZE
+    row_ok && col_ok
+  end
 
-	def squares_all_empty?(squares)
-		squares.all? do |square|
-			piece_at(square) == EMPTY
-		end
-	end
+  def squares_all_empty?(squares)
+    squares.all? do |square|
+      piece_at(square) == EMPTY
+    end
+  end
 
   def place(position, piece)
     @state[position.row][position.col] = piece
@@ -71,10 +71,10 @@ class Board
 
   def move(from, to)
     new_board = Board.clone(self)
-		if from == to
-			return new_board
-		end
-		new_board.moved_pieces << piece_at(from)
+    if from == to
+      return new_board
+    end
+    new_board.moved_pieces << piece_at(from)
     new_board.place(to, piece_at(from))
     new_board.place(from, EMPTY)
     return new_board
@@ -94,11 +94,11 @@ class Board
     return @state[position.row][position.col]
   end
 
-	def pieces
-		state.flatten
-	end
+  def pieces
+    state.flatten
+  end
 
-	private
+  private
 
   def self.clone(board)
     new_board = Board.new()
@@ -112,133 +112,133 @@ class Board
 end
 
 class Threat
-	def self.for_square(mover, square, board)
-		enemies = board.pieces.select do |piece|
-			mover.opponent?(piece)
-		end
+  def self.for_square(mover, square, board)
+    enemies = board.pieces.select do |piece|
+      mover.opponent?(piece)
+    end
 
-		enemies.any? do |piece|
-			potential_boards = piece.moves(board)
-			potential_boards.any? do |potential_board|
-				potential_board.piece_at(square) == piece
-			end
-		end
-	end
+    enemies.any? do |piece|
+      potential_boards = piece.moves(board)
+      potential_boards.any? do |potential_board|
+        potential_board.piece_at(square) == piece
+      end
+    end
+  end
 end
 
 class King < Piece
-	class EastCastle
-		def initialize(king, board)
-			@king = king
-			@board = board
-		end
+  class EastCastle
+    def initialize(king, board)
+      @king = king
+      @board = board
+    end
 
-		def possible_castles
-			castle_at_row(7) + castle_at_row(0)
-		end
+    def possible_castles
+      castle_at_row(7) + castle_at_row(0)
+    end
 
-		private
+    private
 
-		attr_reader :board, :king
+    attr_reader :board, :king
 
-		def castle_at_row(row)
-			legal_castle = rook_in_required_position(row) \
-				&& king_in_required_position(row) \
-				&& empty_castle_path(row) \
-				&& safe_castle_path(row) \
-				&& king_has_not_moved
+    def castle_at_row(row)
+      legal_castle = rook_in_required_position(row) \
+        && king_in_required_position(row) \
+        && empty_castle_path(row) \
+        && safe_castle_path(row) \
+        && king_has_not_moved
 
-			if legal_castle
-				return [execute_castle(row)]
-			end
-			[]
-		end
+      if legal_castle
+        return [execute_castle(row)]
+      end
+      []
+    end
 
-		def execute_castle(row)
-			new_king_position = Position.new(row,6)
-			new_rook_position = Position.new(row,5)
+    def execute_castle(row)
+      new_king_position = Position.new(row,6)
+      new_rook_position = Position.new(row,5)
 
-			board.move(king_position, new_king_position).move(required_rook_position(row), new_rook_position)
-		end
+      board.move(king_position, new_king_position).move(required_rook_position(row), new_rook_position)
+    end
 
-		def king_has_not_moved()
-			!board.moved_pieces.include?(king)
-		end
+    def king_has_not_moved()
+      !board.moved_pieces.include?(king)
+    end
 
-		def safe_castle_path(row)
-			between_spaces = castle_path(row)
-			safe_travel = between_spaces.none? do |position|
-				Threat.for_square(king, position, board)
-			end
-		end
+    def safe_castle_path(row)
+      between_spaces = castle_path(row)
+      safe_travel = between_spaces.none? do |position|
+        Threat.for_square(king, position, board)
+      end
+    end
 
-		def empty_castle_path(row)
-			between_spaces = castle_path(row)
-			board.squares_all_empty?(between_spaces)
-		end
+    def empty_castle_path(row)
+      between_spaces = castle_path(row)
+      board.squares_all_empty?(between_spaces)
+    end
 
-		def king_in_required_position(row)
-			king_position == required_king_position(row)
-		end
+    def king_in_required_position(row)
+      king_position == required_king_position(row)
+    end
 
-		def rook_in_required_position(row)
-			board.piece_at(required_rook_position(row)).castles_with_king(king)
-		end
+    def rook_in_required_position(row)
+      board.piece_at(required_rook_position(row)).castles_with_king(king)
+    end
 
-		def required_rook_position(row)
-			Position.new(row, 7)
-		end
+    def required_rook_position(row)
+      Position.new(row, 7)
+    end
 
-		def required_king_position(row)
-			Position.new(row, 4)
-		end
+    def required_king_position(row)
+      Position.new(row, 4)
+    end
 
-		def king_position
-			board.position_of(king)
-		end
+    def king_position
+      board.position_of(king)
+    end
 
-		def castle_path(row)
-			low, high = [required_king_position(row).col, required_rook_position(row).col].sort
-			((low + 1)...high).map do |col|
-				Position.new(row, col)
-			end
-		end
-	end
+    def castle_path(row)
+      low, high = [required_king_position(row).col, required_rook_position(row).col].sort
+      ((low + 1)...high).map do |col|
+        Position.new(row, col)
+      end
+    end
+  end
 end
 
 class King < Piece
-	def moves(board)
-		current_position = board.position_of(self)
-		one_away_moves = one_away_moves(board)
-		legal_moves = one_away_moves.select do |position|
-			board.on_board(position)
-		end
-
-		not_threatened = legal_moves.reject do |square|
-			Threat.for_square(self, square, board)
-		end
-
-		vanilla_moves = not_threatened.map do |position|
-			board.move(current_position, position)
-		end
-
-		vanilla_moves + EastCastle.new(self, board).possible_castles
-	end
-
-	private
-
-	def one_away_moves(board)
+  def moves(board)
     current_position = board.position_of(self)
-		three_by_three_square = (current_position.row - 1..current_position.row + 1).map do |row|
-			(current_position.col - 1..current_position.col + 1).map do |col|
-				Position.new(row, col)
-			end
-		end.flatten
+    one_away_moves = one_away_moves(board)
+    legal_moves = one_away_moves.select do |position|
+      board.on_board(position)
+    end
 
-		three_by_three_square.reject do |position|
-			position == current_position
-		end
-	end
+    not_threatened = legal_moves.reject do |square|
+      Threat.for_square(self, square, board)
+    end
+
+    vanilla_moves = not_threatened.map do |position|
+      board.move(current_position, position)
+    end
+
+    vanilla_moves + EastCastle.new(self, board).possible_castles
+  end
+
+  private
+
+  def one_away_moves(board)
+    current_position = board.position_of(self)
+    three_by_three_square = (current_position.row - 1..current_position.row + 1).map do |row|
+      (current_position.col - 1..current_position.col + 1).map do |col|
+        Position.new(row, col)
+      end
+    end.flatten
+
+    three_by_three_square.reject do |position|
+      position == current_position
+    end
+  end
 end
 
 class Rook < Piece
@@ -247,65 +247,65 @@ class Rook < Piece
     current_position = board.position_of(self)
     all_positions = row(board) + col(board)
 
-		all_positions.map do |position|
-			board.move(current_position, position)
-		end
+    all_positions.map do |position|
+      board.move(current_position, position)
+    end
   end
 
-	def castles_with_king(king)
-		!opponent?(king)
-	end
+  def castles_with_king(king)
+    !opponent?(king)
+  end
 
-	private
+  private
 
   def col(board)
     current_position = board.position_of(self)
 
-		below = positions_from_rows(((current_position.row + 1)...Board::SIZE), board)
-		above = positions_from_rows((0...current_position.row), board).reverse
+    below = positions_from_rows(((current_position.row + 1)...Board::SIZE), board)
+    above = positions_from_rows((0...current_position.row), board).reverse
 
-		legal_above = take_while_unobstructed(above, board)
-		legal_below = take_while_unobstructed(below, board)
+    legal_above = take_while_unobstructed(above, board)
+    legal_below = take_while_unobstructed(below, board)
 
-		legal_below + legal_above
+    legal_below + legal_above
   end
 
   def row(board)
     current_position = board.position_of(self)
-		to_right = positions_from_columns(((current_position.col + 1)...Board::SIZE), board)
-		to_left = positions_from_columns((0...current_position.col), board).reverse
+    to_right = positions_from_columns(((current_position.col + 1)...Board::SIZE), board)
+    to_left = positions_from_columns((0...current_position.col), board).reverse
 
-		legal_right = take_while_unobstructed(to_right, board)
-		legal_left = take_while_unobstructed(to_left, board)
+    legal_right = take_while_unobstructed(to_right, board)
+    legal_left = take_while_unobstructed(to_left, board)
 
-		legal_left + legal_right
+    legal_left + legal_right
   end
 
-	def take_while_unobstructed(positions, board)
-		positions.reduce([]) do |legal_positions, position|
-			at_position = board.piece_at(position)
-			if at_position == Board::EMPTY && board.squares_all_empty?(legal_positions)
-				legal_positions << position
-			elsif self.opponent?(at_position) && board.squares_all_empty?(legal_positions)
-				legal_positions << position
-			else
-				legal_positions
-			end
-		end
-	end
+  def take_while_unobstructed(positions, board)
+    positions.reduce([]) do |legal_positions, position|
+      at_position = board.piece_at(position)
+      if at_position == Board::EMPTY && board.squares_all_empty?(legal_positions)
+        legal_positions << position
+      elsif self.opponent?(at_position) && board.squares_all_empty?(legal_positions)
+        legal_positions << position
+      else
+        legal_positions
+      end
+    end
+  end
 
-	def positions_from_columns(columns, board)
+  def positions_from_columns(columns, board)
     current_position = board.position_of(self)
-		columns.map do |col|
-			Position.new(current_position.row, col)
-		end
-	end
+    columns.map do |col|
+      Position.new(current_position.row, col)
+    end
+  end
 
-	def positions_from_rows(rows, board)
+  def positions_from_rows(rows, board)
     current_position = board.position_of(self)
-		rows.map do |row|
-			Position.new(row, current_position.col)
-		end
-	end
+    rows.map do |row|
+      Position.new(row, current_position.col)
+    end
+  end
 end
 
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -10,15 +10,15 @@ describe "Acceptance" do
         board.place(Position.new(1, 5), rook)
         accessible_boards = rook.moves(board)
 
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(1,3)) == rook
         end)
 
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(1,4)) == rook
         end)
 
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(0,5)) == rook
         end)
       end
@@ -29,12 +29,12 @@ describe "Acceptance" do
         board.place(Position.new(1, 5), rook)
         accessible_boards = rook.moves(board)
 
-				expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(1,5)) == rook
         end)
       end
 
-			it "cannot move past obstructed space" do
+      it "cannot move past obstructed space" do
         board = Board.new
         black_rook = Rook.new(Board::BLACK)
         white_rook = Rook.new(Board::WHITE)
@@ -45,19 +45,19 @@ describe "Acceptance" do
         board.place(Position.new(5, 1), other_white_rook)
         accessible_boards = black_rook.moves(board)
 
-				expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(1,6)) == black_rook
         end)
 
-				expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(6,1)) == black_rook
         end)
       end
 
-			it "cannot move past obstructed space" do
+      it "cannot move past obstructed space" do
         board = Board.new
         black_rook = Rook.new(Board::BLACK)
-				teammate_black_rook = Rook.new(Board::BLACK)
+        teammate_black_rook = Rook.new(Board::BLACK)
         white_rook = Rook.new(Board::WHITE)
 
         board.place(Position.new(1, 4), black_rook)
@@ -65,166 +65,166 @@ describe "Acceptance" do
         board.place(Position.new(1, 0), white_rook)
         accessible_boards = black_rook.moves(board)
       
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(1,0)) == black_rook
         end)
 
-				expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(1,6)) == black_rook
         end)
       end
     end
-		
-		describe "king" do
-			it "can move one square in any direction" do
+    
+    describe "king" do
+      it "can move one square in any direction" do
         board = Board.new
         king = King.new(Board::BLACK)
         board.place(Position.new(2, 2), king)
-				accessible_boards = king.moves(board)
+        accessible_boards = king.moves(board)
 
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(3,3)) == king
         end)
 
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(1,1)) == king
         end)
 
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(2,3)) == king
         end)
 
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(2,1)) == king
         end)
 
-				expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(1,2)) == king
         end)
       end
 
-			it "cannot move off the board" do
+      it "cannot move off the board" do
         board = Board.new
         king = King.new(Board::BLACK)
         board.place(Position.new(0, 0), king)
-				accessible_boards = king.moves(board)
-				expect(accessible_boards.count).to eq(3)
+        accessible_boards = king.moves(board)
+        expect(accessible_boards.count).to eq(3)
       end
 
-			it "cannot move into check" do
+      it "cannot move into check" do
         board = Board.new
         king = King.new(Board::BLACK)
-				rook = Rook.new(Board::WHITE)
+        rook = Rook.new(Board::WHITE)
         board.place(Position.new(0, 0), king)
         board.place(Position.new(5, 1), rook)
 
-				accessible_boards = king.moves(board)
+        accessible_boards = king.moves(board)
 
-				expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+        expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
           potential_board.piece_at(Position.new(0,1)) == king
         end)
       end
 
-			describe "castling" do
-				it "black king can castle on the left side" do
-					board = Board.new
-					king = King.new(Board::BLACK)
-					rook = Rook.new(Board::BLACK)
-					board.place(Position.new(0, 4), king)
-					board.place(Position.new(0, 7), rook)
+      describe "castling" do
+        it "black king can castle on the left side" do
+          board = Board.new
+          king = King.new(Board::BLACK)
+          rook = Rook.new(Board::BLACK)
+          board.place(Position.new(0, 4), king)
+          board.place(Position.new(0, 7), rook)
 
-					accessible_boards = king.moves(board)
+          accessible_boards = king.moves(board)
 
-					expect(accessible_boards).to include(an_object_satisfying do |potential_board|
-						potential_board.piece_at(Position.new(0, 6)) == king
-						potential_board.piece_at(Position.new(0, 5)) == rook
-					end)
-					expect(accessible_boards.count).to eq(6)
-				end
+          expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+            potential_board.piece_at(Position.new(0, 6)) == king
+            potential_board.piece_at(Position.new(0, 5)) == rook
+          end)
+          expect(accessible_boards.count).to eq(6)
+        end
 
-				it "black king cannot castle with enemy rook" do
-					board = Board.new
-					king = King.new(Board::BLACK)
-					rook = Rook.new(Board::WHITE)
-					board.place(Position.new(0, 4), king)
-					board.place(Position.new(0, 7), rook)
+        it "black king cannot castle with enemy rook" do
+          board = Board.new
+          king = King.new(Board::BLACK)
+          rook = Rook.new(Board::WHITE)
+          board.place(Position.new(0, 4), king)
+          board.place(Position.new(0, 7), rook)
 
-					accessible_boards = king.moves(board)
+          accessible_boards = king.moves(board)
 
-					expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
-						potential_board.piece_at(Position.new(0, 6)) == king
-						potential_board.piece_at(Position.new(0, 5)) == rook
-					end)
-				end
+          expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+            potential_board.piece_at(Position.new(0, 6)) == king
+            potential_board.piece_at(Position.new(0, 5)) == rook
+          end)
+        end
 
-				it "black king cannot castle through pieces" do
-					board = Board.new
-					king = King.new(Board::BLACK)
-					blocking_rook = Rook.new(Board::BLACK)
-					rook = Rook.new(Board::BLACK)
-					board.place(Position.new(0, 4), king)
-					board.place(Position.new(0, 5), blocking_rook)
-					board.place(Position.new(0, 7), rook)
+        it "black king cannot castle through pieces" do
+          board = Board.new
+          king = King.new(Board::BLACK)
+          blocking_rook = Rook.new(Board::BLACK)
+          rook = Rook.new(Board::BLACK)
+          board.place(Position.new(0, 4), king)
+          board.place(Position.new(0, 5), blocking_rook)
+          board.place(Position.new(0, 7), rook)
 
-					accessible_boards = king.moves(board)
+          accessible_boards = king.moves(board)
 
-					expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
-						potential_board.piece_at(Position.new(0, 6)) == king
-						potential_board.piece_at(Position.new(0, 5)) == rook
-					end)
-				end
+          expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+            potential_board.piece_at(Position.new(0, 6)) == king
+            potential_board.piece_at(Position.new(0, 5)) == rook
+          end)
+        end
 
-				it "black king cannot castle through check" do
-					board = Board.new
-					king = King.new(Board::BLACK)
-					checking_rook = Rook.new(Board::WHITE)
-					rook = Rook.new(Board::BLACK)
-					board.place(Position.new(0, 4), king)
-					board.place(Position.new(5, 5), checking_rook)
-					board.place(Position.new(0, 7), rook)
+        it "black king cannot castle through check" do
+          board = Board.new
+          king = King.new(Board::BLACK)
+          checking_rook = Rook.new(Board::WHITE)
+          rook = Rook.new(Board::BLACK)
+          board.place(Position.new(0, 4), king)
+          board.place(Position.new(5, 5), checking_rook)
+          board.place(Position.new(0, 7), rook)
 
-					accessible_boards = king.moves(board)
+          accessible_boards = king.moves(board)
 
-					expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
-						potential_board.piece_at(Position.new(0, 6)) == king
-						potential_board.piece_at(Position.new(0, 5)) == rook
-					end)
-				end
+          expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+            potential_board.piece_at(Position.new(0, 6)) == king
+            potential_board.piece_at(Position.new(0, 5)) == rook
+          end)
+        end
 
-				#what if the rook moves?
-				it "black king cannot castle after it has moved" do
-					board = Board.new
-					king = King.new(Board::BLACK)
-					rook = Rook.new(Board::BLACK)
-					board.place(Position.new(0, 4), king)
-					board.place(Position.new(0, 7), rook)
-					board = board.move(Position.new(0,4), Position.new(1,4))
-					board = board.move(Position.new(1,4), Position.new(0,4))
+        #what if the rook moves?
+        it "black king cannot castle after it has moved" do
+          board = Board.new
+          king = King.new(Board::BLACK)
+          rook = Rook.new(Board::BLACK)
+          board.place(Position.new(0, 4), king)
+          board.place(Position.new(0, 7), rook)
+          board = board.move(Position.new(0,4), Position.new(1,4))
+          board = board.move(Position.new(1,4), Position.new(0,4))
 
-					accessible_boards = king.moves(board)
+          accessible_boards = king.moves(board)
 
-					expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
-						potential_board.piece_at(Position.new(0, 6)) == king
-						potential_board.piece_at(Position.new(0, 5)) == rook
-					end)
-				end
+          expect(accessible_boards).not_to include(an_object_satisfying do |potential_board|
+            potential_board.piece_at(Position.new(0, 6)) == king
+            potential_board.piece_at(Position.new(0, 5)) == rook
+          end)
+        end
 
-				it "white king can castle on the right side" do
-					board = Board.new
-					king = King.new(Board::WHITE)
-					rook = Rook.new(Board::WHITE)
-					board.place(Position.new(7, 4), king)
-					board.place(Position.new(7, 7), rook)
+        it "white king can castle on the right side" do
+          board = Board.new
+          king = King.new(Board::WHITE)
+          rook = Rook.new(Board::WHITE)
+          board.place(Position.new(7, 4), king)
+          board.place(Position.new(7, 7), rook)
 
-					accessible_boards = king.moves(board)
+          accessible_boards = king.moves(board)
 
-					expect(accessible_boards).to include(an_object_satisfying do |potential_board|
-						potential_board.piece_at(Position.new(7, 6)) == king
-						potential_board.piece_at(Position.new(7, 5)) == rook
-					end)
-					expect(accessible_boards.count).to eq(6)
-				end
-			end
-		end
+          expect(accessible_boards).to include(an_object_satisfying do |potential_board|
+            potential_board.piece_at(Position.new(7, 6)) == king
+            potential_board.piece_at(Position.new(7, 5)) == rook
+          end)
+          expect(accessible_boards.count).to eq(6)
+        end
+      end
+    end
   end
 end
